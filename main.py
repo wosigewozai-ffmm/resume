af4a193f4ef2daa2b2edb91354afcdb5e1a20934
yuhongtao <yuhongtaoaaa@163.com>
2021-07-22 17:04:46 +0800

    时间识别并匹配生日和简历信息（时间格式待扩充）

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ for line in open("academydict.txt", "r", encoding='utf-8', errors='ignore'):  # 
     strline = str(line)
     allAcademy.append(strline[0:strline.find(" ")])
 
-doc = Document('myself.docx')
+doc = Document('3.docx')
 
 # 学校列表
 academyList = []
@@ -82,7 +82,17 @@ wordPos = 0
 educateFlag = []
 educateFlagPos = []
 educateNum = 0
-academyDistance = 20
+academyDistance = 5
+
+# 时间列表
+dataList = []
+gapList = []
+dateList = []
+datePoint = 0
+datePos = []
+gapPos = []
+dataPos = []
+dateDistance = 10
 
 #  默认简历中出现的第一个姓名为简历者的姓名
 findName = False
@@ -91,12 +101,37 @@ resultInfo = []  # 提取的结果集
 
 # 每一段的内容
 for para in doc.paragraphs:
-    str = para.text
-    str = " " + str + " "
+    str_ = para.text
+    str_ = " " + str_ + " "
+
+    #  正则匹配提取时间段
+    patter_timeGap = re.compile('\d{4}\.\d{2}[\.\d{2}]*?[\s]*?-[\s]*?\d{4}\.\d{2}[\.\d{2}]*?')
+    Gaps = patter_timeGap.findall(str_)
+    if len(Gaps) > 0:
+        for gap in Gaps:
+            print(gap)
+            gapList.append(gap)
+            dateList.append(gap)
+
+    #  正则匹配提取所有日期
+    patter_date = re.compile(
+        '[^-](\d{4}-\d{2}[-\d{2}]*?)|(\d{4}年\d{2}月[\d{1,2}日]*?)|(\d{4}\.\d{2}[\.\d{2}]*?)|(\d{4}/\d{2}[/\d{2}]*?)[\s]*?[^-]*?')
+    Dates = patter_date.findall(str_)
+    if len(Dates) > 0:
+        for Date in Dates:
+            date = Date[0] + Date[1] + Date[2] + Date[3]
+            dateFlag = True
+            for gap in Gaps:
+                if str(gap).find(str(date)) != -1:
+                    dateFlag = False
+            if dateFlag:
+                print(date)
+                dataList.append(date)
+                dateList.append(date)
 
     # 正则匹配姓名信息
     pattern_name = re.compile('姓[\s]*名[^\u4E00-\u9FFF]*([\u4E00-\u9FFF]*)[^\u4E00-\u9FFF]')
-    person_name = pattern_name.findall(str)
+    person_name = pattern_name.findall(str_)
     if len(person_name) > 0:
         print("姓名：", person_name[0])
         temp = ["姓名", person_name[0]]
@@ -105,7 +140,7 @@ for para in doc.paragraphs:
 
     # 正则匹配身份证号
     pattern_idNumber = re.compile('\D[0-9]{17}[0-9|x|X]\D')
-    id_Number = pattern_idNumber.findall(str)
+    id_Number = pattern_idNumber.findall(str_)
     if len(id_Number) > 0:
         id_Number[0] = id_Number[0][1:len(id_Number[0]) - 1]
         print("身份证号：", id_Number[0])
@@ -113,16 +148,16 @@ for para in doc.paragraphs:
         resultInfo.append(temp)
 
     # 正则匹配手机号
-    pattern_phoneNumber = re.compile('\D([0-9]{3}[-]*?[0-9]{4}[-]*?[0-9]{4})\D')
-    phone_Number = pattern_phoneNumber.findall(str)
+    pattern_phoneNumber = re.compile('\D([0-9]{3})[-]*?([0-9]{4})[-]*?([0-9]{4})\D')
+    phone_Number = pattern_phoneNumber.findall(str_)
     if len(phone_Number) > 0:
-        print("手机号：", phone_Number[0])
-        temp = ["手机号", phone_Number[0]]
+        print("手机号：", phone_Number[0][0] + phone_Number[0][1] + phone_Number[0][2])
+        temp = ["手机号", phone_Number[0][0] + phone_Number[0][1] + phone_Number[0][2]]
         resultInfo.append(temp)
 
     # 正则匹配邮箱信息
     pattern_mailAddress = re.compile('[0-9a-zA-Z.]+@[0-9a-zA-Z.]+?com')
-    mail_Address = pattern_mailAddress.findall(str)
+    mail_Address = pattern_mailAddress.findall(str_)
     if len(mail_Address) > 0:
         print("邮箱：", mail_Address[0])
         temp = ["邮箱", mail_Address[0]]
@@ -130,7 +165,7 @@ for para in doc.paragraphs:
 
     # 性别信息
     pattern_sex = re.compile('[^\u4E00-\u9FFF][男|女][^\u4E00-\u9FFF]')
-    person_sex = pattern_sex.findall(str)
+    person_sex = pattern_sex.findall(str_)
     if len(person_sex) > 0:
         person_sex[0] = person_sex[0][1:len(person_sex[0]) - 1]
         print("性别：", person_sex[0])
@@ -139,7 +174,7 @@ for para in doc.paragraphs:
 
     # 正则匹配年龄信息
     pattern_age = re.compile('\D([0-9]{1,3})岁')
-    person_age = pattern_age.findall(str)
+    person_age = pattern_age.findall(str_)
     if len(person_age) > 0:
         print("年龄：", person_age[0])
         temp = ["年龄", person_age[0]]
@@ -147,7 +182,7 @@ for para in doc.paragraphs:
 
     # 正则匹配籍贯信息
     pattern_nativePlace = re.compile('籍[\s]*贯[^\u4E00-\u9FFF]*([\u4E00-\u9FFF]*)[^\u4E00-\u9FFF]')
-    native_Place = pattern_nativePlace.findall(str)
+    native_Place = pattern_nativePlace.findall(str_)
     if len(native_Place) > 0:
         print("籍贯：", native_Place[0])
         temp = ["籍贯", native_Place[0]]
@@ -155,14 +190,15 @@ for para in doc.paragraphs:
 
     # 正则匹配民族信息
     pattern_Folk = re.compile('民[\s]*族[^\u4E00-\u9FFF]*([\u4E00-\u9FFF]*)[^\u4E00-\u9FFF]')
-    person_Folk = pattern_Folk.findall(str)
+    person_Folk = pattern_Folk.findall(str_)
     if len(person_Folk) > 0:
         print("民族：", person_Folk[0])
         temp = ["民族", person_Folk[0]]
         resultInfo.append(temp)
     #  将段落分词
-    seg_list = jieba.lcut(str, cut_all=False)
-    # 是否存在XX大学/学院
+    temp = str_.split()
+    str_ = "".join(temp)
+    seg_list = jieba.lcut(str_, cut_all=False)
     for seg in seg_list:
         type_seg = psg.cut(seg).__next__().flag
         if type_seg == "nr":
@@ -174,8 +210,8 @@ for para in doc.paragraphs:
                 findName = True
         if type_seg == "es":
             print("英语技能:", seg)
-            pattern_englishSkill = re.compile(seg+'[\D]*([\d]{1,3})[\D]')
-            english_Skill = pattern_englishSkill.findall(str)
+            pattern_englishSkill = re.compile(seg + '[\D]*([\d]{1,3})[\D]')
+            english_Skill = pattern_englishSkill.findall(str_)
             if len(english_Skill) > 0:
                 temp_skill = "{" + seg + "," + english_Skill[0] + "}"
             else:
@@ -183,6 +219,19 @@ for para in doc.paragraphs:
             temp = ["英语技能", temp_skill]
             resultInfo.append(temp)
         wordPos = wordPos + 1
+
+        # 查找日期地址
+        if len(dateList) > datePoint:
+            if str(dateList[datePoint]).find(seg) == 0:
+                datePos.append(wordPos)
+                if len(dateList[datePoint]) > 10:
+                    gapPos.append(wordPos)
+                else:
+                    dataPos.append(wordPos)
+                print(wordPos, " ", dateList[datePoint])
+                datePoint = datePoint + 1
+
+        # 是否存在XX大学/学院
         if seg in allAcademy:
             academyList.append(seg)
             academyPos.append(wordPos)
@@ -191,6 +240,12 @@ for para in doc.paragraphs:
                 educateNum = educateNum + 1
             educateFlag.append(seg)
             educateFlagPos.append(wordPos)
+
+# 不妨假设第一个单独出现的日期是出生日期
+if len(dataList) > 0:
+    print("出生日期:", dataList[0])
+    resultInfo.append(["出生日期", dataList[0]])
+
 # 匹配学历信息
 for i in range(0, len(academyList) - educateNum + 1):
     for j in range(0, len(educateFlag) - educateNum + 1):
@@ -203,9 +258,17 @@ for i in range(0, len(academyList) - educateNum + 1):
             if abs(academyPos[i + k] - educateFlagPos[j + k]) > academyDistance:
                 flag = False
         if flag:
+            dateStart = 0
+            for t in range(0, len(gapList) - educateNum + 1):
+                dateFlag = True
+                for s in range(0, educateNum):
+                    if abs(academyPos[i + s] - gapPos[t + s]) > dateDistance:
+                        dateFlag = False
+                if dateFlag:
+                    dateStart = t
             for k in range(0, educateNum):
-                print(educateFlag[j + k], ":", academyList[i + k])
-                temp = [educateFlag[j + k], academyList[i + k]]
+                print(educateFlag[j + k], ":", academyList[i + k], gapList[dateStart + k])
+                temp = [educateFlag[j + k], academyList[i + k], gapList[dateStart + k]]
                 resultInfo.append(temp)
 
     #  输出到csv表
